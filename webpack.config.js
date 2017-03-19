52d88079e518c3c4f1da9b8ca22a128b4b1a2222
Andrew James Perks Labbe <andrewperkslabbe@gmail.com>
2017-03-18 21:51:30 -0400

    init commit at attempt to achieve code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
 const resolve = require('path').resolve;
 const webpack = require('webpack');
+const ChunkManifestPlugin = require("chunk-manifest-webpack-plugin");
+const WebpackChunkHash = require("webpack-chunk-hash");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
+
+const ASSET_PATH = process.env.ASSET_PATH || '/';
+
+const VENDOR_LIBS = ['react-dom', 'react'];
 
 module.exports = {
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    './index.js'
-  ],
+  entry: {
+      client: [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './index.js'
+    ],
+    vendor: VENDOR_LIBS
+  },
   output: {
-    filename: 'bundle.js',
-
-    path: resolve(__dirname, 'dist'),
-
-    publicPath: '/'
+    path: resolve(__dirname, 'build'),
+    filename: '[name].[chunckhash].js',
+    chunkFilename: '[name].[chunckhash].js',
+    publicPath: ASSET_PATH
   },
 
   context: resolve(__dirname, 'src'),
@@ -23,7 +33,7 @@ module.exports = {
   devServer: {
     hot: true,
     contentBase: resolve(__dirname, 'dist'),
-    publicPath: '/'
+    publicPath: ASSET_PATH
   },
 
   module: {
@@ -47,7 +57,26 @@ module.exports = {
   },
 
   plugins: [
+    new webpack.DefinePlugin({
+        'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH)
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
-  ],
+    new webpack.optimize.CommonsChunkPlugin({
+        name: ['vendor', 'manifest'],
+        miniChunks: Infinity,
+    }),
+    new webpack.HashedModuleIdsPlugin(),
+    new WebpackChunkHash(),
+    new ChunkManifestPlugin({
+      filename: "chunk-manifest.json",
+      manifestVariable: "webpackManifest"
+    }),
+    new HtmlWebpackPlugin({
+        template: './index.ejs'
+    }),
+    new InlineManifestWebpackPlugin({
+       name: 'webpackManifest'
+   })
+  ]
 };
